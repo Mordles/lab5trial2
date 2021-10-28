22eb4701a4a3b1e825edea897c57f445d913aaf1
Mordyg <91845645+Mordles@users.noreply.github.com>
2021-10-28 17:10:57 +0000

    g

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import PCF8591 as ADC
 import time
 import RPi.GPIO as GPIO
 
+ADC.setup[0x48]
+
 GPIO.setmode(GPIO.BCM)
 pins = [18,21,22,23] # controller inputs: in1, in2, in3, in4
 for pin in pins:
@@ -11,36 +13,51 @@ for pin in pins:
 
 
 GPIO.setup(26, GPIO.OUT)
-GPIO.output(26, 1)
+
 
 ccw = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
 [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
+
+state = 0
 
 def delay_us(tus): # use microseconds to improve time resolution
  endTime = time.time() + float(tus)/ float(1E6)
  while time.time() < endTime:
   pass
 
-"""def loop(dir): # dir = rotation direction (cw or ccw)
+"""
+From ADC code  
+
+def loop(dir): # dir = rotation direction (cw or ccw)
  for i in range(512): # full revolution (8 cycles/rotation * 64 gear ratio)
   for halfstep in range(8): # 8 half-steps per cycle
    for pin in range(4):    # 4 pins that need to be energized
     GPIO.output(pins[pin], dir[halfstep][pin])
   delay_us(1000)
 """
+""""
+From ADC code
+
 def setup():
   ADC.setup(0x48)
+""""
 
-"""def loop():
+"""
+From ADC code
+
+def loop():
   while True:
     print(ADC.read(0))
     ADC.write(ADC.read(0))
-"""
+
 
 def destroy():
   ADC.write(0)
+"""
 
-setup()
+#ZERO method 
+GPIO.output(26, 1)
+
 time.sleep(0.1)
 while (ADC.read(0) < 180):
   # move motor to zero
@@ -54,16 +71,43 @@ while (ADC.read(0) < 180):
   ADC.write(ADC.read(0))
 GPIO.output(26, 0)
 GPIO.cleanup()
-  
 
+"""
+#ANGLE method
 
+current_degree = 0
 
+n = input("Enter Degree: ")
+degrees = int(n)
+def angle(degrees):
+  steps = (degrees*512)/360
+  for i in range(steps):
+    for halfstep in range(8):
+      for pin in range(4):
+        GPIO.output(pins[pin], ccw[halfstep][pin])
+      delay_us(1000)
 
+angle(degrees) 
+"""
 
+""""
+From Prof code
 
+def halfstep(dir):
+  # dir +/- 1 (ccw/cw)
+  state += dir
+  if state > 7: state = 0
+  elif state < 0: state = 7
+  for pin in range(4):
+    GPIO.output(pins[pin], sequence[state][pin])
+  delay_us(1000)  
 
+def moveSteps(steps, dir):
+  # move sequence given number of half steps
+  for step in steps:
+    halfstep(dir)
 
-
+"""
 
 
 
@@ -73,6 +117,8 @@ GPIO.cleanup()
 
 
 """
+From ADC code
+
 if __name__ == "__main__":
   try:
     setup()
